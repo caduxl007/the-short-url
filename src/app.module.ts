6a835fc99d1a3314f47d247a555eeb6174b3be70
caduxl007 <eduardo2020dev@gmail.com>
2022-03-18 23:11:33 -0300

    add configs typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UrlsModule } from './modules/urls/urls.module';
 
 @Module({
-  imports: [UrlsModule],
+  imports: [
+    UrlsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5434,
+      username: 'postgres',
+      password: 'docker',
+      database: 'short-url',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
