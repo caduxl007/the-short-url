64d759918085395989f793c7f2d1de3715870231
caduxl007 <eduardo2020dev@gmail.com>
2022-03-18 22:25:04 -0300

    create module urls

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { UrlsModule } from './modules/urls/urls.module';
 
 @Module({
-  imports: [],
+  imports: [UrlsModule],
   controllers: [],
   providers: [],
 })
